de3b18e833e232db7c64edb8a9d450f8da173249
ajibukunoluwa <ibukunajimoti@gmail.com>
2025-03-06 00:23:32 +0000

    Feature: Hide add model button when table does not exist

--- resources/views/show-partial.blade.php
+++ resources/views/show-partial.blade.php
@@ -3,6 +3,13 @@
         <h1 class="text-base font-semibold leading-6 text-gray-900">{{ $modelName }}</h1>
         <p class="mt-2 text-sm text-gray-700">A list of all {{ strtolower($modelName) }} records in your database.</p>
     </div>
+    @if(!isset($error))
+    <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+        <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Add {{ $modelName }}
+        </button>
+    </div>
+    @endif
 </div>
 
 @if(isset($error) && $error === 'table_not_found')
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,20 +1,6 @@
 @extends('modelplus::layouts.app')
 
 @section('content')
-<div class="sm:flex sm:items-center">
-    <div class="sm:flex-auto">
-        <h1 class="text-base font-semibold leading-6 text-gray-900">{{ $modelName }}</h1>
-        <p class="mt-2 text-sm text-gray-700">A list of all {{ strtolower($modelName) }} records in your database.</p>
-    </div>
-    @if(!isset($error))
-    <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-        <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-            Add {{ $modelName }}
-        </button>
-    </div>
-    @endif
-</div>
-
 <div id="table-container">
     @include('modelplus::show-partial')
 </div>
